1d7994567a8b69d4af47c15207e27126a1f04fd1
Sunguk Lee <d3m3vilurr@gmail.com>
2018-06-03 04:06:38 +0900

    Fetch CC recipients

--- office365api/model/message.py
+++ office365api/model/message.py
@@ -3,13 +3,15 @@ from office365api.model.model import Model
 
 class Message(Model):
 
-    select = ['From', 'Subject', 'Body', 'ToRecipients', 'DateTimeReceived', 'HasAttachments']
+    select = ['From', 'Subject', 'Body', 'ToRecipients', 'CcRecipients', 'DateTimeReceived', 'HasAttachments']
 
     def __init__(self, From, ToRecipients, Subject, Body,
-                 HasAttachments=False, Id=None, DateTimeReceived=None):
+                 HasAttachments=False, Id=None, DateTimeReceived=None,
+                 CcRecipients=None):
         self.Id = Id
         self.From = From
         self.ToRecipients = ToRecipients
+        self.CcRecipients = CcRecipients
         self.Subject = Subject
         self.Body = Body
         self.HasAttachments = HasAttachments
